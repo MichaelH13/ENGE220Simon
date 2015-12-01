f8ea463e2d126cfb3728ad89cb9aa8343d1d126f
Michael Higgins <jpayton12@RM106-ST01.mcse.georgefox.edu>
2015-12-01 14:39:57 -0800

    Still working on part 2

--- simon.v
+++ simon.v
@@ -11,10 +11,12 @@ module simon(output [7:0] lcd_data,
 	wire lcd_string_available;
 	wire random;
 	assign enable = ~SimonBtnTL | ~SimonBtnTR | ~SimonBtnBL | ~SimonBtnBR;
-	wire [1:0] btnColor = ~SimonBtnTL ? 0 : ~SimonBtnBL ? 2 : ~SimonBtnTR ? 1 : ~SimonBtnBR ? 3 : -1;
+	wire [1:0] btnColor = tlPressed ? 0 : blPressed ? 2 : trPressed ? 1 : brPressed ? 3 : -1;
 	
 	// NOT IMPLEMENTED YET:
 	wire step, rerun;
+	wire tlPressed, trPressed, blPressed, brPressed;
+	wire tlHeld, trHeld, blHeld, brHeld;
 	assign step = 0;
 	assign rerun = 0;
 	
@@ -38,15 +40,25 @@ module simon(output [7:0] lcd_data,
 		 end
 	  end
 	end
-	
+
+	// DEBOUNCE SIMON BUTTONS
+	// module debouncer(output pressed, held, input button, clk, reset);
+	debouncer tl(.pressed(tlPressed), .held(tlHeld), .button(~SimonBtnTL), .clk(clk), .reset(reset));
+	debouncer tr(.pressed(trPressed), .held(trHeld), .button(~SimonBtnTR), .clk(clk), .reset(reset));
+	debouncer bl(.pressed(blPressed), .held(blHeld), .button(~SimonBtnBL), .clk(clk), .reset(reset));
+	debouncer br(.pressed(brPressed), .held(brHeld), .button(~SimonBtnBR), .clk(clk), .reset(reset));
+
+	// LCD DISPLAY
 	lcd_string lcd(.lcd_regsel(lcd_regsel), .lcd_read(lcd_read), .lcd_enable(lcd_enable), .lcd_data(lcd_data), 
 					.available(lcd_string_available),
 					.print(lcd_string_print), .topline(topline), .bottomline(bottomline), .reset(reset), .clk(clk));
+
 	
 	//	module simon_led_ctrl(output reg [2:0] led0, led1, led2, led3, input[1:0] color, input enable, clk);
 	simon_led_ctrl leds(.led0(led0), .led1(led1), .led2(led2), .led3(led3), .color(btnColor), .enable(enable), .clk(clk));
 	
 	//module LFSR #(parameter FILL=16'hACE1) (output random, input step, rerun, randomize, clk, reset);
-	LFSR shifter(.random(random), .step(step)
+	LFSR shifter(.random(random), .step(step), .rerun(rerun), .randomize(tlHeld), .clk(clk), .reset(reset));
+
 
 endmodule
